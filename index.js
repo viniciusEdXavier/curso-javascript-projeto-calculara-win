8efadb31849c68c522376d78bd93bf01677e76db
viniciusEdXavier <43896829+viniciusEdXavier@users.noreply.github.com>
2019-04-11 20:52:57 -0300

    First Sprint lab

---
+++ index.js
@@ -0,0 +1,8 @@
+let calculator = new CalculatorController();
+
+/*function buttonClick(button)
+{
+    calculator.buttonClick(button);
+}*/
+
+let buttonClick = button => calculator.buttonClick(button);
