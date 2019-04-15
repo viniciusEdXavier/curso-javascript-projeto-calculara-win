31313c3685bd7d8ffe0434162554b75958e55fa9
viniciusEdXavier <43896829+viniciusEdXavier@users.noreply.github.com>
2019-04-14 21:16:10 -0300

    Quarto Sprint casa

--- controller/CalculatorController.js
+++ controller/CalculatorController.js
@@ -16,7 +16,11 @@ class CalculatorController
 
     calculateFields()
     {
-        return eval(this._hiddenNumberString + this._operator + this._displayNumberString);
+        let stringToCalculate = this.hiddenNumberString + this.operator + this.displayNumberString;
+        let result = eval(stringToCalculate);
+        this.updateDisplay(result);
+
+        return result;
     }
 
     updateDisplay(num)
@@ -26,6 +30,7 @@ class CalculatorController
 
     buttonKeyPush(buttonName)
     {
+    
         switch(buttonName)
         {
             case "0":
@@ -38,34 +43,39 @@ class CalculatorController
             case "7":
             case "8":
             case "9":
-                this.displayNumberString = this._displayNumberString+=buttonName;
+                this.displayNumberString +=buttonName;
                 break;
+
             case "+":
             case "-":
             case "÷":
             case "X":
-                if(this._displayNumberString != "")
+                if(this.displayNumberString != "")
                 {
-                    if(this._operator == "")
+                    if(this.operator == "")
                     {
-                        this._hiddenNumberString = this._displayNumberString;
+                        this.hiddenNumberString = this.displayNumberString;
                     }
                     else 
                     {
-                        this._hiddenNumberString = this.calculateFields();
-                        this.displayNumberString = this._hiddenNumberString;
+                        this.hiddenNumberString = this.calculateFields();
                     }
                 }
-                this._operator = buttonName;
-                this._displayNumberString = "";
+                this.operator = buttonName;
+                this.displayNumberString = "";
                 
                 break;
             case "=":
-                this.updateDisplay(this.calculateFields());
+                this.calculateFields();
                 break;
             case "CE":
                 this.displayNumberString = "";
+                this.updateDisplay(0);
                 break;
+            case "C":
+                this.displayNumberString = "";
+                this.hiddenNumberString = "";
+                this.updateDisplay(0)
             default:
                 break;
         }
@@ -74,9 +84,53 @@ class CalculatorController
     set displayNumberString(numberString)
     {
         this._displayNumberString = numberString;
-        if(numberString != "") 
+        if(this._displayNumberString != "") 
         {
             this.updateDisplay(this._displayNumberString);
         }
+    }
+
+    get displayNumberString()
+    {
+        return this._displayNumberString;
+    }
+
+    set operator(buttonText)
+    {
+        const operatorDictionary = {
+            "": "",
+            "+": "+",
+            "-": "-", 
+            "÷": "/", 
+            "X": "*"
+        };
+
+        let operator = operatorDictionary[buttonText];
+
+        //operator !== undefined ? this._operator = operator : () => {throw new Error("Operador não cadastrado!")};
+        
+        if(operator !== undefined) 
+        {
+            this._operator = operator;
+        }
+        else
+        {
+            throw new Error("Operador não cadastrado!");
+        }
+    }
+
+    get operator()
+    {
+        return this._operator;
+    }
+
+    set hiddenNumberString(hiddenNumberString)
+    {
+        this._hiddenNumberString = hiddenNumberString;
+    }
+
+    get hiddenNumberString()
+    {
+        return this._hiddenNumberString;
     }
 }
